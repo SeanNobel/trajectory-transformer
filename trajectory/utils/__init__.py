4d96dd7653761d8ce51a9ce431b618ee4808920d
Sensho Nobe <75618251+SeanNobel@users.noreply.github.com>
2022-01-16 16:44:21 +0900

    Update __init__.py

--- trajectory/utils/__init__.py
+++ trajectory/utils/__init__.py
@@ -1,3 +1,4 @@
+"""
 from .setup import Parser, watch
 from .arrays import *
 from .serialization import *
@@ -6,3 +7,4 @@ from .rendering import make_renderer
 # from .video import *
 from .config import Config
 from .training import Trainer
+"""
